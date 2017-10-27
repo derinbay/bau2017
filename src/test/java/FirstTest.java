9a7fe5a8da557d70f01573d5f31b738ec3db6806
Taylan Derinbay <taylan.derinbay@n11.com>
2017-10-27 16:01:37 +0300

    add shouldLoginAndSearch test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,13 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,36 +15,34 @@ import static org.junit.Assert.assertTrue;
 
 public class FirstTest {
 
-    @Test
-    public void myFirstTest() {
+    WebDriver driver;
+    String keyword = "samsung";
+
+    @Before
+    public void startUp() {
 //        System.setProperty("webdriver.chrome.driver", "/Users/taylan/Downloads/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.n11.com");
+    }
 
-        WebElement searchBar = driver.findElement(By.id("searchData"));
-        searchBar.sendKeys("samsung");
-
-        WebElement searchButton = driver.findElement(By.className("iconSearch"));
-        searchButton.click();
-
+    @After
+    public void tearDown() {
         driver.close();
     }
 
     @Test
+    public void myFirstTest() {
+        WebElement searchBar = driver.findElement(By.id("searchData"));
+        searchBar.sendKeys(keyword);
+
+        WebElement searchButton = driver.findElement(By.className("iconSearch"));
+        searchButton.click();
+    }
+
+    @Test
     public void shouldLogin() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://www.n11.com");
-
-        String keyword = "Samsung";
-
-        WebElement loginButton = driver.findElement(By.className("btnSignIn"));
-        loginButton.click();
-
-        driver.findElement(By.id("email")).sendKeys("bau@bau.com");
-        driver.findElement(By.id("password")).sendKeys("123qwe");
-        driver.findElement(By.id("loginButton")).click();
+        login();
 
         String userName = driver.findElement(By.className("username")).getText();
         assertThat("When a buyer logged in", userName, equalTo("Test Bau"));
@@ -55,7 +57,41 @@ public class FirstTest {
         String text = driver.findElement(By.xpath("//*[@class='resultText']/h1")).getText();
         driver.findElement(By.xpath("//*[@class='group setGroup keywordBannerGroup']//li[1]/a"));
         assertTrue(resultText.equals(keyword));
+    }
 
-        driver.close();
+    private void login() {
+        WebElement loginButton = driver.findElement(By.className("btnSignIn"));
+        loginButton.click();
+
+        driver.findElement(By.id("email")).sendKeys("bau@bau.com");
+        driver.findElement(By.id("password")).sendKeys("123qwe");
+        driver.findElement(By.id("loginButton")).click();
+    }
+
+    @Test
+    public void shouldLoginAndSearch() {
+        login();
+
+        String userName = driver.findElement(By.className("username")).getText();
+        assertThat("When a buyer logged in", userName, equalTo("Test Bau"));
+
+        WebElement searchBar = driver.findElement(By.id("searchData"));
+        searchBar.sendKeys(keyword);
+
+        WebElement searchButton = driver.findElement(By.className("iconSearch"));
+        searchButton.click();
+
+        List<WebElement> productList = driver.findElements(By.cssSelector("#view .productName"));
+        List<WebElement> productList1 = driver.findElements(By.xpath("//*[@id='view']//*[contains(@class, 'productName')]"));
+
+        for (WebElement product : productList) {
+            String productTitle = product.getText().toLowerCase();
+            System.out.println("Asserting: " + productTitle);
+            assertTrue(productTitle.contains(keyword));
+        }
+
+//        for (int i=0; i < productList.size(); i++) {
+//            assertTrue(productList.get(i).getText().toLowerCase().contains(keyword));
+//        }
     }
 }
