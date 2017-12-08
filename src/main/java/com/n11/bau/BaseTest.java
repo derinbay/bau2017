7f11d58382fa595bb37e748532bebefc6e30a907
Taylan Derinbay <taylan.derinbay@n11.com>
2017-12-08 15:28:16 +0300

    add grid parameteres

--- src/main/java/com/n11/bau/BaseTest.java
+++ src/main/java/com/n11/bau/BaseTest.java
@@ -6,7 +6,11 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -17,10 +21,10 @@ public class BaseTest {
     public TestName testName = new TestName();
 
     @Before
-    public void startUp() {
+    public void startUp() throws MalformedURLException {
 //        System.setProperty("webdriver.chrome.driver", "/Users/taylan/Downloads/chromedriver");
         System.out.println(testName.getMethodName());
-        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
         driver.manage().window().maximize();
         driver.get("http://www.n11.com");
     }
